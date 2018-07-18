3478fffabc9f3650c42161978f355bf0bbce371f
administrator <dyadav225@gmail.com>
2018-07-18 14:58:28 +0530

    testing purpose hr

--- app/src/main/java/com/example/administrator/kotlinfirebasedemo/MainActivity.kt
+++ app/src/main/java/com/example/administrator/kotlinfirebasedemo/MainActivity.kt
@@ -81,7 +81,7 @@ class MainActivity : AppCompatActivity() {
         if (mRemoteConfig!!.getInfo().configSettings.isDeveloperModeEnabled) {
             cacheExpiration = 0
         }
-//i am changing the data new datatattat
+//i am changing the data new datatattatjdjjf
         //this is check to test the githun
 
         mRemoteConfig!!.fetch(cacheExpiration)
